bd2ee432a0a14e2f498dd95015a4af0def9200bc
AmberChaoYang <chaoyang@seas.upenn.edu>
2021-04-05 00:14:16 -0400

    Update database_endpoint.py

--- database_endpoint.py
+++ database_endpoint.py
@@ -22,9 +22,6 @@ from sqlalchemy.orm import load_only
 # In[2]:
 
 
-run models.py
-
-
 # In[3]:
 
 
@@ -108,7 +105,7 @@ def trade():
         order['sell_amount'] = payload['sell_amount']
         order['signature'] = sk
         
-        
+
         #order_obj = Order( sender_pk=order['sender_pk'],receiver_pk=order['receiver_pk'],                          buy_currency=order['buy_currency'], sell_currency=order['sell_currency'],                           buy_amount=order['buy_amount'], sell_amount=order['sell_amount'],signature = order['signature'])
 
         #g.session.add(order_obj)
